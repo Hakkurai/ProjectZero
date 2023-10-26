0624b07740c48f0e7c92b5d27ce1c9f0267b7a36
Hakkurai <gparagas2003@gmail.com>
2023-10-27 05:15:55 +0800

    Leaderboard added

--- PROJECT ZERO/Assets/Scripts/LeaderBoard.cs
+++ PROJECT ZERO/Assets/Scripts/LeaderBoard.cs
@@ -1,41 +1,92 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using Mono.Data.Sqlite;
+using UnityEditor.MemoryProfiler;
 using UnityEngine;
-using TMPro;
-using Dan.Main;
-public class LeaderBoard : MonoBehaviour
+using UnityEngine.UI;
+
+public class Leaderboard : MonoBehaviour
 {
-    [SerializeField]
-    private List<TextMeshProUGUI> names;
+    public InputField foodInput;
+    public InputField mealInput;
+    public Text foodList;
 
-    [SerializeField]
-    private List<TextMeshProUGUI> scores;
-
-    private string publicLeaderboardKey = "c795898e60ee5ab423b0f4bd2465f55a345e9dcf576bcfaee70e48a187471f9c";
-
-    private void Start()
+    private string dbName = "URI=file:Leaderboard.db";
+    // Start is called before the first frame update
+    void Start()
     {
-        GetLeaderboard();
+        CreateDB();
+
+        DisplayLeaderboard();
+
     }
 
-    public void GetLeaderboard()
+    public void CreateDB()
     {
-        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) =>
+        using (var connection = new SqliteConnection(dbName))
         {
-            int loopLength = (msg.Length < names.Count) ? msg.Length : names.Count;
-            for (int i = 0; i < loopLength; ++i)
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
             {
-                names[i].text = msg[i].Username;
-                scores[i].text = msg[i].Score.ToString();
+                command.CommandText = "CREATE TABLE IF NOT EXISTS Leaderboard (Name VARCHAR(30), Score INT);";
+                command.ExecuteNonQuery();
             }
-        }));
+
+            connection.Close();
+        }
     }
 
-    public void SetLeaderboardEntry(string username, int score)
+    public void AddPlayer()
     {
-        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username, score, ((msg) =>
+        using (var connection = new SqliteConnection(dbName))
         {
-            GetLeaderboard();
-        }));
-    }
-}
+            connection.Open();
 
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO Leaderboard (Name, Score) VALUES ('" + foodInput.text + "', '" + mealInput.text + "');";
+                command.ExecuteNonQuery();
+            }
+
+            connection.Close();
+        }
+        DisplayLeaderboard();
+    }
+
+    public void DisplayLeaderboard()
+    {
+        foodList.text = "";
+
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM Leaderboard ORDER BY Score DESC"; 
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string name = reader["Name"].ToString();
+                        string score = reader["Score"].ToString();
+                        int nameWidth = 30; 
+                        int paddingWidth = nameWidth - name.Length;
+                        string padding = new string(' ', Math.Max(0, paddingWidth));
+                        string formattedLine = $"{name}{padding}\t{score}";
+                        foodList.text += formattedLine + "\n";
+                    }
+
+                    reader.Close();
+                }
+            }
+
+            connection.Close();
+        }
+    }
+
+}
